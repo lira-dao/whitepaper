5cc619152e83b9feebc876f40555bf4309569162
Thelroth <thelroth@satoshilira.io>
2024-05-10 11:54:47 +0200

    config update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -66,7 +66,7 @@ const config: Config = {
       items: [
         {
           type: 'docSidebar',
-          sidebarId: 'whitepaperSidebar',
+          sidebarId: 'MainSidebar',
           position: 'left',
           label: 'Whitepaper',
         },
